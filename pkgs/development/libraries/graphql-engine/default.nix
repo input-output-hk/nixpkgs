01326ae36b832284373052ebfe3de11eba3e510e
Craige McWhirter <craige@mcwhirter.io>
2019-10-25 07:12:45 +1000

    Switched to using output from cabal2nix

--- pkgs/development/libraries/graphql-engine/default.nix
+++ pkgs/development/libraries/graphql-engine/default.nix
@@ -1,56 +1,11 @@
-{ stdenv
-, fetchFromGitHub
-, nodejs
-, google-cloud-sdk
-, brotli
-, libpqxx
-, python3
-}:
+{ haskellPackages }:
 
-stdenv.mkDerivation rec {
-  pname = "graphql-engine";
-  version = "1.0.0-beta.7";
-
-  src = fetchFromGitHub {
-    owner = "hasura";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "18aim5ww0drjr546gb7p6jvjhm41sy2x7cy9padnq3svacs4kzfs";
-  };
-
-  nativeBuildInputs = [
-    nodejs
-    python3
-  ];
-
-  buildInputs = [
-    brotli
-    google-cloud-sdk
-    libpqxx
-  ];
-
-  checkInputs = [
-  ];
-
-  databaseoCheck = true;
-
-  preConfigure =
-    ''
-    '';
-
-  buildPhase = ''
-    runHook preBuild
-    cd console
-    npm ci
-    npm run server-build
-    cd ..
-  '';
-
-  meta = with stdenv.lib; {
-    description = "GraphQL server that gives you instant, realtime GraphQL APIs over Postgres, with webhook triggers on database events, and remote schemas";
-    homepage = https://github.com/hasura/graphql-engine/;
-    license = licenses.asl20;
-    maintainers = with maintainers; [ craigem ];
-    platforms = platforms.linux;
+haskellPackages.override {
+  overrides = self: super: {
+    ci-info = self.callPackage ./ci-info.nix {};
+    graphql-engine = self.callPackage ./graphql-engine.nix {};
+    graphql-parser = self.callPackage ./graphql-parser.nix {};
+    #multi-ghc-travis = self.callPackage ./multi-ghc-travis.nix {};
+    pg-client = self.callPackage ./pg-client.nix {};
   };
 }
